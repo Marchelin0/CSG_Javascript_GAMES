95312f794c572a84abd6a5ff54590e77f3b88a95
Marchelin0 <153053@csgaugustinus.nl>
2022-11-30 21:09:01 +0000

    asd

--- mijnGameMarchelino/JS/Game.js
+++ mijnGameMarchelino/JS/Game.js
@@ -3,7 +3,7 @@ import TileMap from "./TileMap.js";
 const tileSize = 32;
 const canvas= document.getElementById("gameCanvas");
 const ctx = canvas.getContext ('2d');
-const  tileMap = new TileMap(tileSize);
+const tileMap = new TileMap(tileSize);
 
 function gameLoop(){
     tileMap.draw(ctx);
